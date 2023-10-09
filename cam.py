fd63c258061bd7eb1ee36d23e7f4244e9788023c
Matthew Hung <matthewhung09@gmail.com>
2023-10-09 15:00:10 -0700

    save output to another file

--- cam.py
+++ cam.py
@@ -3,17 +3,29 @@ import cv2
 
 dicOption={'rtsp_transport':'tcp'}
 video = av.open('***REMOVED***', 'r', options=dicOption)
+output = av.open('cam.mp4', 'w', format='mp4')
+
+in_stream = video.streams.video[0]
+out_stream = output.add_stream(template=in_stream)
 
 try:
-    for packet in video.demux():
+    for packet in video.demux(in_stream):
         print(packet)
+
         for frame in packet.decode():
-            if packet.stream.type == 'video':
-                print(frame)
-                img = frame.to_ndarray(format='bgr24')
-                cv2.imshow("Video", img)
+            print(frame)
+            img = frame.to_ndarray(format='bgr24')
+            cv2.imshow("Video", img)
+
+        packet.stream = out_stream
+
+        output.mux(packet)
+
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+        
 except KeyboardInterrupt:
     pass
+
+output.close()
 cv2.destroyAllWindows()
