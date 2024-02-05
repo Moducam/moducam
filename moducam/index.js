af202670bb04310ab46f3773d0e95cd05a5ab9a7
Matthew Hung <matthewhung09@gmail.com>
2024-02-05 14:31:39 -0800

    working version with pipe:

--- moducam/index.js
+++ moducam/index.js
@@ -4,10 +4,8 @@ const { spawn } = require('child_process');
 const CONFIG_PATH = '../config.ini';
 const WebSocket = require("ws");
 
-const io = require('socket.io')
-
 let config = ini.parse(fs.readFileSync(CONFIG_PATH, 'utf-8'))
-var pythonProcess = spawn('/opt/homebrew/opt/python@3.11/bin/python3.11', ['../cam.py', CONFIG_PATH]);
+var pythonProcess = spawn('/opt/homebrew/opt/python@3.11/bin/python3.11', ['../cam.py', CONFIG_PATH, true]);
 setProcessEvents();
 
 const wss = new WebSocket.Server({
@@ -18,16 +16,51 @@ wss.on("connection", function connection(ws) {
     console.log("Client conneted to websocket");
 });
 
-// listen for the new image
-pythonProcess.stdout.on("data", (data) => {
-    // broadcast the new binary image to all clients
-    wss.clients.forEach((client) => {
-        if (client.readyState === WebSocket.OPEN) {
-            client.send(data, { binary: true });
+const path = 'my_pipe';
+let fifo = spawn('mkfifo', [path])
+
+fifo.on('exit', function(status) {
+    console.log('Created Pipe');
+
+    const fd  = fs.openSync(path, 'r+');
+    let fifoRs = fs.createReadStream(null, { fd });
+
+    console.log('Ready to write')
+
+    image = ''
+    image = Buffer.alloc(0);
+
+    fifoRs.on('data', data => {
+        image = Buffer.concat([image, data]);
+
+        // 2196 should be last packet
+        // if (image.length == 2418836) {
+        if (data.length < 8192) {
+            console.log('sending');
+            wss.clients.forEach((client) => {
+                if (client.readyState === WebSocket.OPEN) {
+                    client.send(image, {binary : true});
+                }
+            });
+            image = Buffer.alloc(0);
         }
     });
-    io.emit('image', data)
 });
+
+// listen for the new image
+// pythonProcess.stdout.on("data", (data) => {
+//     console.log(data)
+//     let buf = Buffer.from(data);
+//     console.log(buf)
+//     fs.writeFileSync("new-path.jpg", data);
+
+//     // broadcast the new binary image to all clients
+//     wss.clients.forEach((client) => {
+//         if (client.readyState === WebSocket.OPEN) {
+//             client.send(base64);
+//         }
+//     });
+// });
 
 exports.restart = function() {
     if (pythonProcess) {
